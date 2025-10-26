5c92ba34588a35499f4a06efd208738b037f70c8
Kriss-Kross <kwamefosu33@gmail.com>
2025-10-26 17:42:27 +0000

    add middleware for player repository and refactor database connection handling

--- backend/routes/db/postgresql/_middleware.dart
+++ backend/routes/db/postgresql/_middleware.dart
@@ -1,21 +1,16 @@
 import 'dart:io';
 
 import 'package:dart_frog/dart_frog.dart';
-import 'package:postgres/postgres.dart';
+import 'package:stormberry/stormberry.dart';
+
+final db = Database(
+  host: Platform.environment['BACKEND_HOST'] ?? 'localhost',
+  database: Platform.environment['POSTGRES_DB'] ?? 'postgres',
+  username: Platform.environment['POSTGRES_USER'] ?? 'postgres',
+  password: Platform.environment['POSTGRES_PASSWORD'] ?? 'postgres',
+  useSSL: false,
+);
 
 Handler middleware(Handler handler) {
-  return (context) async {
-    final connection = await Connection.open(
-      Endpoint(
-        host: Platform.environment['BACKEND_HOST'] ?? 'localhost',
-        database: Platform.environment['POSTGRES_DB'] ?? 'postgres',
-        username: Platform.environment['POSTGRES_USER'] ?? 'postgres',
-        password: Platform.environment['POSTGRES_PASSWORD'] ?? 'postgres',
-      ),
-    );
-    final response =
-        handler.use(provider<Connection>((_) => connection)).call(context);
-    await connection.close();
-    return response;
-  };
+  return handler.use(provider<Database>((_) => db));
 }
