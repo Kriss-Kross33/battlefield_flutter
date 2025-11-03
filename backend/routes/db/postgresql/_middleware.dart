cf7f15dc0e7049982a558ca1c40b72f6c330a51e
Kriss-Kross <kwamefosu33@gmail.com>
2025-11-03 01:47:30 +0000

    refactor database connection parameters in middleware for consistency

--- backend/routes/db/postgresql/_middleware.dart
+++ backend/routes/db/postgresql/_middleware.dart
@@ -4,10 +4,10 @@ import 'package:dart_frog/dart_frog.dart';
 import 'package:stormberry/stormberry.dart';
 
 final db = Database(
-  host: Platform.environment['BACKEND_HOST'] ?? 'localhost',
-  database: Platform.environment['POSTGRES_DB'] ?? 'postgres',
-  username: Platform.environment['POSTGRES_USER'] ?? 'postgres',
-  password: Platform.environment['POSTGRES_PASSWORD'] ?? 'postgres',
+  host: Platform.environment['DB_HOST_ADDRESS'],
+  database: Platform.environment['DB_NAME'],
+  username: Platform.environment['DB_USERNAME'],
+  password: Platform.environment['DB_PASSWORD'],
   useSSL: false,
 );
 
